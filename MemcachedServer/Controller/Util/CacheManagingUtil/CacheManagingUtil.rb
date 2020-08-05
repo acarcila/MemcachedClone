f49f5ddaf2971328e84167c4e5ff1092c51906ca
acarcila <acarcila98@gmail.com>
2020-08-05 17:31:25 -0500

    Se cambió el nombre del método startKeyManaging a createKeyManagingThread

--- MemcachedServer/Controller/Util/CacheManagingUtil/CacheManagingUtil.rb
+++ MemcachedServer/Controller/Util/CacheManagingUtil/CacheManagingUtil.rb
@@ -3,7 +3,7 @@ require_relative "../../../Model/Item/Item"
 
 class CacheManagingUtil
   # creates a Thread that deletes the expired keys every second
-  def CacheManagingUtil.startKeyManaging(cache)
+  def CacheManagingUtil.createKeyManagingThread(cache)
     thread = Thread.new do
       while true
         cache.deleteExpiredKeys(currentTime: Time.now)
