7ac56b19e30fe101900f65528e2782ade5b5772c
acarcila <acarcila98@gmail.com>
2020-08-05 11:49:05 -0500

    Se corrigió el regex de número entero

--- MemcachedServer/Controller/Util/Constants/CommandConstants.rb
+++ MemcachedServer/Controller/Util/Constants/CommandConstants.rb
@@ -7,7 +7,7 @@ class CommandConstants
   APPEND = "append"
   PREPEND = "prepend"
   CAS = "cas"
-  INTEGER_REGEX = /\d+/
+  INTEGER_REGEX = /\d+\b/
   GET_GETS_REGEX = /#{GET}(s|)\b/
   NOT_GET_REGEX = /#{SET}|#{ADD}|#{REPLACE}|#{APPEND}|#{PREPEND}|#{CAS}\b/
   VALID_COMMAND_REGEX = /(#{SET}|#{ADD}|#{REPLACE}|#{APPEND}|#{PREPEND}|#{CAS}|(#{GET}(s|)))\b/
