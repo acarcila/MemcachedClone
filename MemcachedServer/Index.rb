0c9b13d541300bd2077a2c1179fb71fbc0c68500
acarcila <acarcila98@gmail.com>
2020-08-05 17:36:36 -0500

    Se implementó la clase TCPUtil en el index

--- MemcachedServer/Index.rb
+++ MemcachedServer/Index.rb
@@ -5,59 +5,42 @@ require_relative "Controller/Util/Constants/ResponseConstants"
 require_relative "Controller/Util/CommandTranslateUtil/CommandTranslateUtil"
 require_relative "Controller/Util/CommandExecuteUtil/CommandExecuteUtil"
 require_relative "Controller/Util/CacheManagingUtil/CacheManagingUtil"
+require_relative "Controller/Util/TCPUtil/TCPUtil"
 require_relative "Model/Cache/Cache"
 
 exit if defined?(Ocra)
 port = ARGV[0]
 
-until (port =~ CommandConstants::INTEGER_REGEX)
+until (port =~ CommandConstants::INTEGER_REGEX && TCPUtil.checkPortAvailability(port))
   puts ResponseConstants::SELECT_PORT
   port = STDIN.gets.strip
 end
 
 cache = Cache.new
-server = TCPServer.new port
 
 $stdout.sync = true
 
 threads = []
-tcpThread = Thread.new do
-  puts ResponseConstants::SERVER_CONNECTED_TEMPLATE % port
-  loop do
-    Thread.start(server.accept) do |client|
-      until client.eof?
-        msg = (client.gets).strip
-        mapCommand = CommandTranslateUtil.translateCommand(msg)
-        unless mapCommand[CommandPartsConstants::COMMAND] =~ CommandConstants::GET_GETS_REGEX || mapCommand[CommandPartsConstants::STATUS] =~ ResponseConstants::ERROR_REGEX
-          value = (client.gets).gsub(/\r\n/, "")
-          until value.size >= mapCommand[CommandPartsConstants::WHITESPACE]
-            value += "\r\n#{((client.gets).gsub(/\r\n/, ""))}"
-          end
-        end
-
-        responseArray = CommandExecuteUtil.execute(mapCommand, cache, value)
-        client.puts("#{responseArray.shift}\r\n") until responseArray.empty?
-      end
+tcpThread = TCPUtil.createTCPThread(port, lambda { |client|
+  msg = (client.gets).strip
+  if msg == CommandConstants::QUIT
+    client.close
+    return true
+  end
+  mapCommand = CommandTranslateUtil.translateCommand(msg)
+  unless mapCommand[CommandPartsConstants::COMMAND] =~ CommandConstants::GET_GETS_REGEX || mapCommand[CommandPartsConstants::STATUS] =~ ResponseConstants::ERROR_REGEX
+    value = StringUtil.cleanString(client.gets)
+    until value.size >= mapCommand[CommandPartsConstants::WHITESPACE]
+      value += "\r\n#{StringUtil.cleanString(client.gets)}"
     end
   end
-end
 
-cacheThread = CacheManagingUtil.startKeyManaging(cache)
+  responseArray = CommandExecuteUtil.execute(mapCommand, cache, value)
+  client.puts("#{responseArray.shift}\r\n") until responseArray.empty?
+  return false
+})
+
+cacheThread = CacheManagingUtil.createKeyManagingThread(cache)
 threads = [tcpThread, cacheThread]
 
 threads.each(&:join)
-
-# def cleanString(string)
-#   (string).gsub(/\r\n/, "")
-# end
-
-# def checkPortAvailability(port)
-#   begin
-#     checkServer = TCPServer.new port
-#     checkServer.close
-#   rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
-#     return false
-#   end
-
-#   return true
-# end
