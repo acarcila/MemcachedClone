7dfae0be37d496e1d732005983782b129d8954d1
acarcila <acarcila98@gmail.com>
2020-08-04 22:34:42 -0500

    Se agregaron los templates para las respuestas

--- MemcachedServer/Controller/Util/Constants/ResponseConstants.rb
+++ MemcachedServer/Controller/Util/Constants/ResponseConstants.rb
@@ -8,6 +8,11 @@ class ResponseConstants
   SERVER_ERROR = "SERVER_ERROR"
   LINE_FORMAT_ERROR = "bad command line format"
   BAD_DATA_ERROR = "bad data chunk"
+  SERVER_CONNECTED_TEMPLATE = "Server started in port: %d"
+  ITEM_TEMPLATE = "VALUE %s %d %d"
+  ITEM_CAS_TEMPLATE = "VALUE %s %d %d %d"
+  VALUE_TEMPLATE = "%s"
+  ERROR_TEMPLATE = "%s %s"
   END_ = "END"
   ERROR_REGEX = /ERROR/
 end
